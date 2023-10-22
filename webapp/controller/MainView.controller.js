870e66e5c95cbb2795ae18b4a533e6998f89adf9
jasmindedic <jdedic1999@gmail.com>
2023-10-22 11:13:46 +0200

    Implemented api and tested it

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -10,7 +10,26 @@ sap.ui.define([
 
         return Controller.extend("Zweatherapp.weatherapp.controller.MainView", {
             onInit: function () {
+                let weatherData = new JSONModel("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=rain&hourly=rain&daily=apparent_temperature_max&timezone=auto");
+                this.getView().setModel(weatherData, "weatherModel");
 
-            }
+                const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=rain&hourly=rain&daily=apparent_temperature_max&timezone=auto";
+
+                /* Test */
+                this.test(url);
+                /* Test */
+            }, 
+
+            /* Test */
+            test: function(url){
+                fetch(url)
+                .then((response) => response.json())
+                .then(data => {
+                    console.log("Api data:")
+                    console.log(data);
+                })
+            },
+            /* Test */
+
         });
     });
